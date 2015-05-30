ee1f8689d0be60d1a9da2bec55ce223d5ed5f37f
Sorin Davidoi <sorin.davidoi@gmail.com>
2015-05-30 15:04:36 +0200

    Revert "Copy startCGMOVIEPROJECT.sh into the right directory"
    This reverts commit a5bf32057dff1d94320410edb556d9b0fe7436c6.

--- exercises/CGMOVIEPROJECT/startCGMOVIEPROJECT.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PATH=$PATH:/home/sorin/.clion10/system/cmake/generated/bb0c0fb9/bb0c0fb9/MinSizeRel/bin:/usr/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sorin/.clion10/system/cmake/generated/bb0c0fb9/bb0c0fb9/MinSizeRel/lib:/usr/lib64:/home/sorin/.clion10/system/cmake/generated/bb0c0fb9/bb0c0fb9/MinSizeRel/lib
-CGMOVIEPROJECT $*
-#read -p "Press any key to continue"
