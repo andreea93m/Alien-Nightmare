0eb614949029f525db2338739621de0eba71929e
Sorin Davidoi <sorin.davidoi@gmail.com>
2015-06-19 22:24:31 +0200

    Modify characters positions in ChaseScene

--- exercises/CGMOVIEPROJECT/src/ChaseScene.cpp
+++ exercises/CGMOVIEPROJECT/src/ChaseScene.cpp
@@ -10,8 +10,8 @@
 AlienNightmare::ChaseScene::ChaseScene(Position position, Size size) : Scene(position, size),
 																		germany(1, 0, "textures/germany.png"),
 																		sun(Position(this, 0.5, 0.5, 0.5), 1),
-																		animal(Position(this, 0.2, 0.15, 0.2), 1,1,1.5, this),
-																		warrior(Position(this, 0.5, 0.2, 0.2), Size(1, 1, 1), this),
+																		animal(Position(this, 0.1, 0.15, 0.2), 1,1,1.5, this),
+																		warrior(Position(this, 0.6, 0.2, 0.2), Size(1, 1, 1), this),
 																		demoObject(Position(this, 0.5, 0.5, 0.5),
                                                                                   Size(1, 1, 1)) {
 	germany.placeRelative(this, 0.8, 0, 0);
